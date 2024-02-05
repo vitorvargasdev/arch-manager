e84a0898ac182b4f324511bdc1dfb58df4f22800
vitorvargasdev <instalando@protonmail.com>
2024-02-04 21:14:14 -0300

    refactor: populateKeys function to handle errors and provide more informative error messages

--- pkgs/install/populate-keys.go
+++ pkgs/install/populate-keys.go
@@ -12,6 +12,7 @@ func installArchLinuxKeyring() (string, error) {
 
 	if err := cmd.Run(); err != nil {
 		fmt.Println("could not install archlinux-keyring: ", err)
+		return "", err
 	}
 
 	return "", nil
@@ -23,6 +24,7 @@ func initializePacmanKeyring() (string, error) {
 
 	if err := cmd.Run(); err != nil {
 		fmt.Println("could not initialize pacman-keyring: ", err)
+		return "", err
 	}
 
 	return "", nil
@@ -34,13 +36,27 @@ func populatePacmanKeyring() (string, error) {
 
 	if err := cmd.Run(); err != nil {
 		fmt.Println("could not populate pacman-keyring: ", err)
+		return "", err
 	}
 
 	return "", nil
 }
 
-func populateKeys() {
-	installArchLinuxKeyring()
-	initializePacmanKeyring()
-	populatePacmanKeyring()
+func populateKeys() (string, error) {
+	if _, err := installArchLinuxKeyring(); err != nil {
+		fmt.Println("Error installing archlinux-keyring: ", err)
+		return "", err
+	}
+
+	if _, err := initializePacmanKeyring(); err != nil {
+		fmt.Println("Error initializing pacman-keyring: ", err)
+		return "", err
+	}
+
+	if _, err := populatePacmanKeyring(); err != nil {
+		fmt.Println("Error populating pacman-keyring: ", err)
+		return "", err
+	}
+
+	return "done", nil
 }
